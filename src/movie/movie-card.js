9b9e10029cf7e8b9e82594fbdd85de8e6fa0fd00
mahdislimane <56697088+mahdislimane@users.noreply.github.com>
2020-04-17 16:45:10 +0200

    second commit

--- src/movie/movie-card.js
+++ src/movie/movie-card.js
@@ -1,13 +1,20 @@
 import React from "react";
-import Rater from "react-rater";
-import "react-rater/lib/react-rater.css";
+import StarRatingComponent from "react-star-rating-component";
 
 export default function MovieCard(props) {
   return (
     <div className="movieCard">
       <h3>{props.movieCard.name}</h3>
       <img className="pic" src={props.movieCard.pic} alt=""></img>
-      <Rater rating={props.movieCard.rating} total={5} interactive={false} />
+      <StarRatingComponent
+        name={props.movieCard.name}
+        value={props.movieCard.rating}
+        starCount={"5"}
+        starColor={"#ffb400"}
+        emptyStarColor={"#333"}
+        editing={false}
+      />
+
       <h4>{props.movieCard.year}</h4>
     </div>
   );
