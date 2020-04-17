c8acac200e22d0c64d1d2d5fa3912fe57942712f
mahdislimane <slimane_mahdi@yahoo.fr>
2020-04-17 16:09:15 +0100

    Updates

---
+++ precache-manifest.2dccaa2f7ffdc27d805fd587d5587a08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511513253a29c5c97a481e59c019a08a",
+    "url": "/LVL2checkpoint007/index.html"
+  },
+  {
+    "revision": "31315e40434e35c03550",
+    "url": "/LVL2checkpoint007/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "65b74761bf959b0d2297",
+    "url": "/LVL2checkpoint007/static/css/main.9afc85dd.chunk.css"
+  },
+  {
+    "revision": "31315e40434e35c03550",
+    "url": "/LVL2checkpoint007/static/js/2.fd9c80e5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/LVL2checkpoint007/static/js/2.fd9c80e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b74761bf959b0d2297",
+    "url": "/LVL2checkpoint007/static/js/main.dd56f0dd.chunk.js"
+  },
+  {
+    "revision": "0e7e05c325c1f2319691",
+    "url": "/LVL2checkpoint007/static/js/runtime-main.8defa0ba.js"
+  }
+]);
