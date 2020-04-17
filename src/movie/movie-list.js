6ce10c249796d042afb2d813234fcd65b1ecc7bf
mahdislimane <56697088+mahdislimane@users.noreply.github.com>
2020-04-17 16:44:16 +0200

    second commit

--- src/movie/movie-list.js
+++ src/movie/movie-list.js
@@ -2,23 +2,40 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 
 export default function MovieList(props) {
+  let rateSearchPlus = props.rateSearch + 1;
   let filtred = props.movieList.filter((el) => el.rating >= props.rateSearch);
+  let refiltred = filtred.filter((el) => el.rating < rateSearchPlus);
   return (
     <div>
-      {filtred.map((el) => {
-        return (
-          <div>
-            <Button
-              style={{ width: 280 }}
-              onClick={() => {
-                props.setmovieCard(el);
-              }}
-            >
-              {el.name}
-            </Button>
-          </div>
-        );
-      })}
+      {props.rateSearch
+        ? refiltred.map((el) => {
+            return (
+              <div>
+                <Button
+                  style={{ width: 280 }}
+                  onClick={() => {
+                    props.setmovieCard(el);
+                  }}
+                >
+                  {el.name}
+                </Button>
+              </div>
+            );
+          })
+        : props.movieList.map((el) => {
+            return (
+              <div>
+                <Button
+                  style={{ width: 280 }}
+                  onClick={() => {
+                    props.setmovieCard(el);
+                  }}
+                >
+                  {el.name}
+                </Button>
+              </div>
+            );
+          })}
     </div>
   );
 }
