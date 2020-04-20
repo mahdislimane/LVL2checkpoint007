feb1af08c5328fca6ab506816caf5cf9b1c06e22
mahdislimane <56697088+mahdislimane@users.noreply.github.com>
2020-04-20 16:36:51 +0200

    third commit

--- src/movie/movie-card.js
+++ src/movie/movie-card.js
@@ -1,21 +1,33 @@
 import React from "react";
 import StarRatingComponent from "react-star-rating-component";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import "../App.css";
 
 export default function MovieCard(props) {
   return (
-    <div className="movieCard">
-      <h3>{props.movieCard.name}</h3>
-      <img className="pic" src={props.movieCard.pic} alt=""></img>
-      <StarRatingComponent
-        name={props.movieCard.name}
-        value={props.movieCard.rating}
-        starCount={"5"}
-        starColor={"#ffb400"}
-        emptyStarColor={"#333"}
-        editing={false}
-      />
+    <Modal show={props.show} onHide={props.handleClose}>
+      <Modal.Header closeButton></Modal.Header>
+      <Modal.Body>
+        <div className="movieCard" style={{ fontSize: "30px" }}>
+          <img className="pic" src={props.movieCard.pic} alt=""></img>
+          <StarRatingComponent
+            name={props.movieCard.name}
+            value={props.movieCard.rating}
+            starCount={"5"}
+            starColor={"#ffb400"}
+            emptyStarColor={"#333"}
+            editing={false}
+          />
 
-      <h4>{props.movieCard.year}</h4>
-    </div>
+          <h4>{props.movieCard.year}</h4>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={props.handleClose}>
+          OK
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
--- src/movie/movie-list.js
+++ src/movie/movie-list.js
@@ -2,40 +2,46 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 
 export default function MovieList(props) {
-  let rateSearchPlus = props.rateSearch + 1;
   let filtred = props.movieList.filter((el) => el.rating >= props.rateSearch);
-  let refiltred = filtred.filter((el) => el.rating < rateSearchPlus);
+
   return (
-    <div>
-      {props.rateSearch
-        ? refiltred.map((el) => {
-            return (
-              <div>
-                <Button
-                  style={{ width: 280 }}
-                  onClick={() => {
-                    props.setmovieCard(el);
-                  }}
-                >
-                  {el.name}
-                </Button>
-              </div>
-            );
-          })
-        : props.movieList.map((el) => {
-            return (
-              <div>
-                <Button
-                  style={{ width: 280 }}
-                  onClick={() => {
-                    props.setmovieCard(el);
-                  }}
-                >
-                  {el.name}
-                </Button>
-              </div>
-            );
-          })}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        flexWrap: "wrap",
+      }}
+    >
+      {filtred.map((el) => {
+        return (
+          <div
+            style={{
+              flexDirection: "column",
+              display: "flex",
+              alignItems: "center",
+              margin: "20px",
+              cursor: "pointer",
+            }}
+          >
+            <h2
+              onClick={() => {
+                props.setmovieCard(el);
+                props.setShowsec(true);
+              }}
+            >
+              {el.name}
+            </h2>
+            <img
+              style={{ height: "350px" }}
+              src={el.pic}
+              onClick={() => {
+                props.setmovieCard(el);
+                props.setShowsec(true);
+              }}
+            ></img>
+          </div>
+        );
+      })}
     </div>
   );
 }
